d0748ad91c5bb10376672a5c7aef01750dc6663e
Swapnil Kumar Singh <124446138+usernameswapnil@users.noreply.github.com>
2025-06-26 16:00:42 +0530

    Update connection.js

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://Admin69:swapnilhero123@cluster1-shard-00-00.rpirq.mongodb.net:27017,cluster1-shard-00-01.rpirq.mongodb.net:27017,cluster1-shard-00-02.rpirq.mongodb.net:27017/dispensary-System?replicaSet=atlas-tmwehx-shard-0&ssl=true&authSource=admin").then(res=>{
+mongoose.connect("mongodb+srv://Admin69:mongodbswapnil%40123@cluster1.rpirq.mongodb.net/dispensary-System?retryWrites=true&w=majority").then(res=>{
     console.log("Database Connected Successfully");
 }).catch(err=>{
     console.log(err);
